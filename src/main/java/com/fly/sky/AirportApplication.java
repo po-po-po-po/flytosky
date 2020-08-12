367b22c20ba8bccec5ca0a6fb97c3ed6bda22f0b
wangzekun <wangzekun@bonc.com.cn>
2020-08-12 17:20:04 +0800

    提交代码 部署方式为war包部署

--- src/main/java/com/fly/sky/AirportApplication.java
+++ src/main/java/com/fly/sky/AirportApplication.java
@@ -3,6 +3,8 @@ package com.fly.sky;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,11 +17,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @SpringBootApplication
 @MapperScan("com.fly.sky.repository")//扫描的mapper
-public class AirportApplication {
+public class AirportApplication  extends SpringBootServletInitializer {
     public static void main(String[] args) {
         //启动spring应用程序
         SpringApplication.run(AirportApplication.class,args);
     }
 
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AirportApplication.class);
+    }
 }
