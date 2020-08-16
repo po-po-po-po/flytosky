9c85bc851bf6789e7bc89b17a2a982aa123cb6e1
po <564987175@qq.com>
2020-08-16 08:33:18 +0800

    注释掉爬取定时任务配置

--- src/main/java/com/fly/sky/pythons/SynchronizeTask.java
+++ src/main/java/com/fly/sky/pythons/SynchronizeTask.java
@@ -19,16 +19,15 @@ import java.util.TimerTask;
  * author: wangzekun
  * version: 1.0
  */
-@Configuration      //1.主要用于标记配置类，兼备Component的效果。
-@EnableScheduling   // 2.开启定时任务
+//@Configuration      //1.主要用于标记配置类，兼备Component的效果。
+//@EnableScheduling   // 2.开启定时任务
 public class SynchronizeTask{
     @Autowired
     FlightService flightService;
 
     //3.添加定时任务
-    @Scheduled(cron = "0/20 * * * * ?")
+    //@Scheduled(cron = "0/15 * * * * ?")
     //或直接指定时间间隔，例如：40秒
-    //@Scheduled(fixedRate=5000)
     private void configureTasks() {
         System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
         FlightCondition condition=new FlightCondition();
