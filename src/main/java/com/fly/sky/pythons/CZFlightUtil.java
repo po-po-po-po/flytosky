ca8d7c0ccb4342e0ffefd44693979d75ee827248
po <564987175@qq.com>
2020-08-23 02:06:46 +0800

    南航指定code爬取数据

--- src/main/java/com/fly/sky/pythons/CZFlightUtil.java
+++ src/main/java/com/fly/sky/pythons/CZFlightUtil.java
@@ -123,4 +123,73 @@ public class CZFlightUtil {
     }
 
 
+
+
+    @Test
+    @Rollback(false)
+    public  void  scrableCZByCode() throws InterruptedException {
+        //抓取CZ地址
+        String url="https://b2c.csair.com/portal/flight/direct/query";
+        //获取机场列表
+        AirportCode airportCode=new AirportCode();
+        //airportCode.setDeptCode("PEK");
+
+            //每请求一次休息5秒
+            Thread.currentThread().sleep(5000);
+            //请求的参数是：
+            CZParam CZParam = new CZParam("CAN", "JXA", "20200920");
+            String jsonCZ = JSONObject.toJSONString(CZParam);
+            log.info("爬取南航网站请求的参数是：" + jsonCZ);
+            //利用ip代理访问 获取代理ip和端口
+            // String ipAndPort[] = IpPortUtil.getIpAndPort();
+            String ipAndPort[] = {"49.232.228.221", "9998"};
+            if(ipAndPort!=null){
+                //String content = HttpRequestUtils.sendPost(ipAndPort[0], ipAndPort[1], url, jsonCZ,0);
+                String content = HttpRequestUtils.sendPost(ipAndPort[0], ipAndPort[1],url, jsonCZ,1);
+                //解析爬取南航的数据
+                CZData1 cZData1 = new JSONObject().parseObject(content, CZData1.class);
+                System.out.println(cZData1);
+                if (null == cZData1.getData()) {
+                 //   log.info("从机场三字码" + airport1.getDeptCode()+"到机场三字码" +airport1.getArrCode()+"没有爬取到数据");
+                } else {
+                 //   log.info("从机场三字码" + airport1.getDeptCode()+"到机场三字码" +airport1.getArrCode()+"成功爬取到数据");
+                    List<CZData5> list = cZData1.getData().getSegment().get(0).getDateFlight().getFlight();
+                    log.info("爬取到的南航数据是：" + list);
+                    //进行组装数据
+                    for (CZData5 czData5 : list) {
+                        if (!czData5.isCodeShare()) {
+                            Flight flight = new Flight();
+                            flight.setAirportNameEnd("");
+                            flight.setAirlinesCode("CZ");
+                            flight.setFlightNo(czData5.getFlightNo());
+                            StringBuffer flightDate = new StringBuffer(czData5.getDepTime());
+                            flightDate.insert(2, ":");
+                            flightDate.append("-");
+                            flightDate.append(czData5.getArrTime());
+                            flightDate.insert(8, ":");
+                            flight.setFlightDate(flightDate.toString());
+                            flight.setAirportNameStartCode(czData5.getDepPort());
+                            flight.setAirportNameEndCode(czData5.getArrPort());
+                            if(!"无".equals(czData5.getStopNameZh().trim())){
+                                flight.setFlightRemark("(经停" + czData5.getStopNameZh().trim() + ")");
+                                flight.setFlightRequency("(经停" + czData5.getStopNameZh().trim() + ")");
+                            }
+                            //根据机场code查询机场数据
+                            Airport arr = airportRepository.findAirportByCode(czData5.getArrPort());
+                            Airport dep = airportRepository.findAirportByCode(czData5.getDepPort());
+                            flight.setFlightNameEnd(arr.getAirportAbbreviate() + RegexUtil.filterAlphabetAndNumber(czData5.getArrivalTerminal()));
+                            flight.setFlightNameStart(dep.getAirportAbbreviate() + RegexUtil.filterAlphabetAndNumber(czData5.getDepartureTerminal()));
+                            flight.setAirportNameStart(dep.getAirportAbbreviate());
+                            flight.setAirportNameEnd(arr.getAirportAbbreviate());
+                            log.info("入库数据是：" + flight);
+                            flightRepository.insertFlight(flight);
+                        }
+                    }
+                }
+            }
+            //爬取完数据 需要改变状态
+
+    }
+
+
 }
