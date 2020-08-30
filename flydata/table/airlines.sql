5576dd391fe4b02e17637266c2c7a396de6403ab
po <564987175@qq.com>
2020-08-30 16:37:26 +0800

    更新龙江航空

--- flydata/table/airlines.sql
+++ flydata/table/airlines.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50649
 File Encoding         : 65001
 
-Date: 2020-08-30 16:33:08
+Date: 2020-08-30 16:36:54
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,7 +29,7 @@ CREATE TABLE `airlines` (
   `airlines_abbreviate` varchar(255) DEFAULT NULL,
   `airlines_rank` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of airlines
@@ -66,3 +66,4 @@ INSERT INTO `airlines` VALUES ('29', '九元航空', 'AQ', null, 'https://www.po
 INSERT INTO `airlines` VALUES ('30', '福州航空', 'FU', null, 'https://www.potucs.com/wechat/airline/fu.png', null, '福州航空', null);
 INSERT INTO `airlines` VALUES ('31', '桂林航空', 'GT', null, 'https://www.potucs.com/wechat/airline/gt.png', null, '桂林航空', null);
 INSERT INTO `airlines` VALUES ('32', '大新华航空', 'CN', null, 'https://www.potucs.com/wechat/airline/hu.png', null, '大新华航空', null);
+INSERT INTO `airlines` VALUES ('33', '龙江航空', 'LT', null, 'https://www.potucs.com/wechat/airline/lt.png', null, '龙江航空', null);
