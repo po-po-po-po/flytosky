ccac712ac3cf388e9d29a3b73078184bbdcd2087
po <564987175@qq.com>
2020-09-13 14:23:31 +0800

    机场工具类

---
+++ src/main/java/com/fly/sky/scrable/domain/jsonairport/Airport0.java
@@ -0,0 +1,18 @@
+package com.fly.sky.scrable.domain.jsonairport;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * description: Airport
+ * date: 2020/9/12 16:19
+ * author: wangzekun
+ * version: 1.0
+ */
+@Setter
+@Getter
+public class Airport0 {
+    private List<Airport1> airport;
+}
--- src/test/java/fly/AirportCodeTest.java
+++ src/test/java/fly/AirportCodeTest.java
@@ -49,10 +49,10 @@ public class AirportCodeTest {
         List<Airport> airportsList1=airportRepository.findAirportsByCondition(new AirportCondition());
         for (Airport airport : airportsList1) {
                     AirportCode airportCode=new AirportCode();
-                    airportCode.setDeptCode("KWL");
+                    airportCode.setDeptCode("NGQ");
                     airportCode.setArrCode(airport.getAirportCode());
                     airportCode.setStatus("0");
-                    airportCode.setDeptName("桂林");
+                    airportCode.setDeptName("阿里");
                     airportCode.setArrName(airport.getAirportLocation());
                     log.info("插入机场code："+airportCode);
                     airportCode.setStatus(null);
@@ -72,10 +72,10 @@ public class AirportCodeTest {
             for (Airport airport : airportsList2) {
                 AirportCode airportCode=new AirportCode();
                 airportCode.setDeptCode(airport.getAirportCode());
-                airportCode.setArrCode("KWL");
+                airportCode.setArrCode("NGQ");
                 airportCode.setStatus("0");
                 airportCode.setDeptName(airport.getAirportLocation());
-                airportCode.setArrName("桂林");
+                airportCode.setArrName("阿里");
                 log.info("插入机场code："+airportCode);
                 airportCode.setStatus(null);
                 List<AirportCode> airportCodesList= airportCodeRepository.findAirportCode(airportCode);
