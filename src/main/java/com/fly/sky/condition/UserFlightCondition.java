f8b47ee9491ce07aa11283c436dceafbcc3b3799
wangzekun <wangzekun@bonc.com.cn>
2020-09-27 20:05:46 +0800

    用户航班列表和添加

--- src/main/java/com/fly/sky/condition/UserFlightCondition.java
+++ src/main/java/com/fly/sky/condition/UserFlightCondition.java
@@ -18,7 +18,21 @@ import java.util.Date;
 @ToString
 public class UserFlightCondition extends PagedCondition{
 
-    @ApiModelProperty("openid")
+    @ApiModelProperty("航班号")
+    private String flightNo;
+    @ApiModelProperty("出发机场")
+    private String flightNameStart;
+    @ApiModelProperty("到达机场")
+    private String flightNameEnd;
+    @ApiModelProperty("航班日期")
+    private String flightDate;
+    @ApiModelProperty("航空公司代码")
+    private String airlinesCode;
+    @ApiModelProperty("航班频率")
     private String openid;
+    @ApiModelProperty("创建时间")
+    private Date createTime;
+    @ApiModelProperty("航班状态:0有效 1 无效")
+    private Integer status;
 
 }
