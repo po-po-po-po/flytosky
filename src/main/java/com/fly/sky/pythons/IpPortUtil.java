e1c09844c5a772a5acb4a4be43fcefd2f27e5c4b
wangzekun <wangzekun@bonc.com.cn>
2021-05-06 11:06:12 +0800

    提交配置文件

--- src/main/java/com/fly/sky/pythons/IpPortUtil.java
+++ src/main/java/com/fly/sky/pythons/IpPortUtil.java
@@ -26,7 +26,7 @@ public class IpPortUtil {
     //获取代理ip和端口
     public static String[] getIpAndPort() {
 
-        String urlPath="http://http2.9vps.com/getip.asp?username=13522715896&pwd=26bb7a0367dc214015af04c4f1c5b8a0&geshi=1&fenge=1&fengefu=&getnum=100";
+        String urlPath="http://http2.9vps.com/getip.asp?username=13522715896&pwd=87e9a749eb8e5e5d02bc952b10b6e8bd&geshi=1&fenge=1&fengefu=&getnum=100";
         // 首先抓取异常并处理
         String returnString = "1";
         try{
